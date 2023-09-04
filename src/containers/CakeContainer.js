f52c89df48db95a6f9438a12c67ba4b84c66d4c7
amelieboydshire <aboydshire10@gmail.com>
2023-09-04 20:56:37 +0100

    added cake container

---
+++ src/containers/CakeContainer.js
@@ -0,0 +1,50 @@
+
+const CakeContainer = () => {
+
+    const [cakeRecipes, setCakeRecipes] = useState([
+{
+	cakeName: "Victoria Sponge",
+   	ingredients: [
+   		"eggs",
+      	"butter",
+      	"sugar",
+      	"self-raising flour",
+      	"baking powder",
+      	"milk"
+  	],
+  	price: 5,
+   	rating: 5
+},
+{
+ 	cakeName: "Tea Loaf",
+   	ingredients: [
+   		"eggs",
+      	"oil",
+      	"dried fruit",
+      	"sugar",
+      	"self-raising flour",
+      	"strong tea",
+  	],
+  	price: 2,
+  	rating: 3
+},
+{
+ 	cakeName: "Carrot Cake",
+   	ingredients: [
+    	"carrots",
+      	"walnuts",
+      	"oil",
+      	"cream cheese",
+      	"flour",
+      	"sugar",
+   	],
+   	price: 8,
+   	rating: 5
+}
+    ]);
+
+
+
+
+
+}
